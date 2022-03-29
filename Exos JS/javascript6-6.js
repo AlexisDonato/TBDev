d857cd5905aae7e3c53256772a154c21c8a24a47
Alexis <alek6.don@gmail.com>
2022-03-29 16:57:02 +0200

    Suite Exos JS

--- Exos JS/javascript6-6.js
+++ Exos JS/javascript6-6.js
@@ -87,10 +87,6 @@ else {
 */
 
 
-
-
-
-
 // var btn = document.createElement("BUTTON");        // Créer un élément <button>
 // var t = document.createTextNode("CLICK ME");       // Créer un noeud textuel
 // btn.appendChild(t);                                // Ajouter le texte au bouton
@@ -109,8 +105,139 @@ else {
 // btn0.style.color = "DarkRed";
 
 
-function checkForm(f) 
+
+
+
+
+
+
+
+let a =  document.getElementsByTagName("aside");
+    for (i=0; i<a.length; i++)
+    {
+        a[i].style = "flex: 1; align-content: flex-start; display: inline-block;";
+    }
+
+
+let a2 =  document.getElementsByTagName("aside2");
+    for (i=0; i<a2.length; i++)
+    {
+        a2[i].style = "flex: 1; align-content: flex-start;";
+    }    
+
+let a3 =  document.getElementsByClassName("ast");
+for (i=0; i<a3.length; i++)
 {
-  alert("Vous vous appelez : " + f.elements['nom'].value + " " + f.elements['prenom'].value);
-  return false; // do not submit the form
-}
+    a3[i].style = "font-size: 1rem; font-weight: bold; color: #FF6666;";
+}    
+
+    
+let l =  document.getElementsByTagName("label");
+    for (i=0; i<l.length; i++)
+    {
+        l[i].style = "font-size: 1em; font-weight: bold; vertical-align: middle;";
+    }
+
+
+    document.getElementsByClassName("row")[0].style = "display: flex;"; 
+    document.getElementsByClassName("boutons")[0].style = "text-align: center;";
+
+    document.getElementsByTagName("legend")[0].style = "font-size: 2rem; font-weight: bold;";
+    document.getElementsByTagName("legend2")[0].style = "font-size: 1.5rem; font-weight: bold; color: red;";
+    
+
+    document.getElementById("environnement").addEventListener("click", event =>
+    {
+        document.getElementsByClassName("choix")[0].style = "display: none;";
+    })    
+
+
+function checkForm(event) 
+        { 
+                let soc = document.getElementById("societe");
+                let per = document.getElementById("personne");
+                let cp = document.getElementById("cp");
+                let vil = document.getElementById("ville");
+                let mail = document.getElementById("email");
+                let tel = document.getElementById("tel");
+
+            if (soc.validity.valueMissing)
+            {
+                event.preventDefault();
+                alert("Veuillez renseigner le nom de la société s.v.p. !");
+                return false; // n’envoie pas le formulaire
+            }
+
+            else if (per.validity.valueMissing)
+            {
+                event.preventDefault();
+                alert("Veuillez renseigner le nom de la personne à contacter s.v.p. !");
+                return false; // n’envoie pas le formulaire
+            }
+
+            else if (vil.validity.valueMissing)
+            {
+                event.preventDefault();
+                alert("Veuillez renseigner le nom de la ville s.v.p. !");
+                return false; // n’envoie pas le formulaire
+            }
+
+            else if (!soc.match(/^[A-Za-z]+$/))
+            {
+                event.preventDefault();
+                alert("Nom de société invalide !");
+                return false;
+            }
+
+            else if (!per.match(/^[A-Za-z]+$/))
+            {
+                event.preventDefault();
+                alert("Nom de personne invalide !");
+                return false;
+            }
+
+            else if (!vil.match(/^[A-Za-z]+$/))
+            {
+                event.preventDefault();
+                alert("Nom de ville invalide !");
+                return false;
+            }
+
+            else if (cp.length<5)
+            {
+                event.preventDefault();
+                alert("Entrez le code postal sur 5 chiffres s.v.p. !");
+                return false;
+            }
+
+            else if (!cp.match(/^[0-9]*$/))
+            {
+                event.preventDefault();
+                alert("Entrez un code postal valide s.v.p. !");
+                return false;
+            }
+
+            else if (!mail.match(/^(([^<()[\]\\.,;:\s@\]+(\.[^<()[\]\\.,;:\s@\]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/))
+            {
+                event.preventDefault();
+                alert("Veuillez renseigner une adresse mail valide s.v.p. !");
+                return false;
+            }
+
+            else if (!tel.match("^0[1-9]([-. ]?[0-9]{2}){4}$"))
+            {
+                event.preventDefault();
+                alert("Veuillez renseigner un numéro de téléphone valide s.v.p. !");
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+
+document.getElementById("submit").addEventListener("click", checkForm);
+
+
+
